baee5223687720f145f07b4847026ca0535e6161
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-21 23:12:39 +0300

    feat: import components to App

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import React from 'react';
+import {Header} from './components/Header';
+import {Search} from './components/Search';
+import {Categories} from './components/Categories';
+import {Main} from './components/Main';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    return (
+        <View style={styles.container}>
+            <Header/>
+            <Search/>
+            <Categories/>
+            <Main/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        paddingHorizontal: 15,
+        backgroundColor: '#ffeedb'
+    }
 });
