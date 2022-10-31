ed40cab9a699ed321ad227374222af1aec9db02c
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-31 22:17:37 +0200

    refactor: remove extra component imports

--- App.tsx
+++ App.tsx
@@ -1,8 +1,5 @@
 import {StyleSheet, View} from 'react-native';
 import React from 'react';
-import {Header} from './components/Header';
-import {Search} from './components/Search';
-import {Categories} from './components/Categories';
 import {Main} from './components/Main';
 
 
@@ -10,9 +7,6 @@ export default function App() {
 
     return (
         <View style={styles.container}>
-            <Header/>
-            <Search/>
-            <Categories/>
             <Main/>
         </View>
     );
